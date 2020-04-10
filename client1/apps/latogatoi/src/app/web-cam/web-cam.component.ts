550dfd1e719900b6df7c552fff2de2427049352f
saimonjy <saimonjy@gmail.com>
2020-04-10 16:02:18 +0200

    generaltam egy uj komponenst, WebCam neven

---
+++ client1/apps/latogatoi/src/app/web-cam/web-cam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'client1-web-cam',
+  templateUrl: './web-cam.component.html',
+  styleUrls: ['./web-cam.component.css']
+})
+export class WebCamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
