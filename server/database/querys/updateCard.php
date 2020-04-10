16665f2aa37d95d527709e413c73a75988be22e2
saimonjy <saimonjy@gmail.com>
2020-04-10 15:34:15 +0200

    newCard.php-hez es a update.php-hez hozza adtam a parameter ellenorzo fg-t

--- server/database/querys/updateCard.php
+++ server/database/querys/updateCard.php
@@ -2,18 +2,16 @@
 require(dirname(dirname(__DIR__)) . '/includes/config.php');
 
 $post = json_decode(file_get_contents('php://input'), true);
-// print_r($post);
 
-//1. $post atalakitasa olyan adatta ami a tablaban jo (ervenyesseg kezelese)
 if (!$_GET['id']) {
     $result = ['error' => [
         'message' => 'ervenytelen azonosito: '. !$_GET['id'],
     ]]; 
     die(json_encode($result));
 }
+$db = new Database();
+$db->safeSQLParams($post);
 $query = "UPDATE `Card` SET `elotag`='".$post["elotag"]."', `vezeteknev`='".$post['vezeteknev']."', `keresztnev`='".$post['keresztnev']."', `intezmeny_nev`='".$post['intezmeny_nev']."', `rendfokozat`='".$post['rendfokozat']."', `img`='".$post['img']."' WHERE `id`='".$_GET['id']."'";
 
-//2. $post eltarolasa az adatbazisban
-$db = new Database();
 echo $db->save($query);
 ?>
