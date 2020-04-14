73af1d4c94ff53d0e3ed1f3ab89455fa91141164
János Csikós <csikosjanos@gmail.com>
2020-04-15 00:17:48 +0100

    Modulok atrendezese

--- client1/apps/latogatoi/src/app/app.module.ts
+++ client1/apps/latogatoi/src/app/app.module.ts
@@ -1,51 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {WebcamModule} from 'ngx-webcam';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {LatogatoiCsoportokModule} from './latogatoi-csoportok/latogatoi-csoportok.module';
-import {KartyakModule} from './kartyak/kartyak.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LatogatoiCsoportokModule } from './latogatoi-csoportok/latogatoi-csoportok.module';
+import { KartyakModule } from './kartyak/kartyak.module';
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    WebcamModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatGridListModule,
-    MatListModule,
-    MatButtonModule,
     MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
     LatogatoiCsoportokModule,
-    KartyakModule,
+    KartyakModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client1/apps/latogatoi/src/app/kartyak/kartyak.module.ts
+++ client1/apps/latogatoi/src/app/kartyak/kartyak.module.ts
@@ -1,17 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import { WebcamModule } from 'ngx-webcam';
+
 import { CardListComponent } from './card-list/card-list.component';
 import { NewCardComponent } from './new-card/new-card.component';
 import { PrintViewComponent } from './print-view/print-view.component';
 
 @NgModule({
-  declarations: [
-    CardListComponent, 
-    NewCardComponent, 
-    PrintViewComponent,
-  ],
+  declarations: [CardListComponent, NewCardComponent, PrintViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    WebcamModule
   ]
 })
-export class KartyakModule { }
+export class KartyakModule {}
