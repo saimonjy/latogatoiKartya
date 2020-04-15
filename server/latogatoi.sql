f45a31ff153592b65410d8a4a23424261e04792c
János Csikós <csikosjanos@gmail.com>
2020-04-15 18:24:25 +0100

    Vezeteknev vissza allitasa, prod konfig, db export

--- server/latogatoi.sql
+++ server/latogatoi.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Gép: localhost
--- Létrehozás ideje: 2020. Ápr 14. 22:48
--- Kiszolgáló verziója: 10.1.39-MariaDB
--- PHP verzió: 7.1.29
+-- Host: localhost
+-- Generation Time: Apr 15, 2020 at 07:21 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,30 +19,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `latogatoi`
+-- Database: `latogatoi`
 --
+CREATE DATABASE IF NOT EXISTS `latogatoi` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `latogatoi`;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `Card`
---
-
-CREATE TABLE `Card` (
-  `id` int(11) NOT NULL,
-  `elotag` varchar(5) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `vezeteknev` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
-  `keresztnev` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
-  `intezmeny_nev` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `img` longtext COLLATE utf8_hungarian_ci,
-  `rendfokozat` varchar(30) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `latogatoi_csoport` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `felhasznalok`
+-- Table structure for table `felhasznalok`
 --
 
 CREATE TABLE `felhasznalok` (
@@ -50,74 +35,102 @@ CREATE TABLE `felhasznalok` (
   `jelszo` varchar(32) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- Dumping data for table `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`nev`, `jelszo`) VALUES
+('hello', 'cd880b726e0a0dbd4237f10d15da46f4');
+
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `latogatoi_csoport`
+-- Table structure for table `kartyak`
 --
 
-CREATE TABLE `latogatoi_csoport` (
+CREATE TABLE `kartyak` (
+  `id` int(11) NOT NULL,
+  `vezeteknev` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
+  `keresztnev` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
+  `img` longtext COLLATE utf8_hungarian_ci,
+  `rendfokozat` varchar(30) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `latogatoi_csoport` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Dumping data for table `kartyak`
+--
+
+INSERT INTO `kartyak` (`id`, `vezeteknev`, `keresztnev`, `img`, `rendfokozat`, `latogatoi_csoport`) VALUES
+(7, 'asdasdf', '', '', '', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `latogatoi_csoportok`
+--
+
+CREATE TABLE `latogatoi_csoportok` (
   `id` int(11) NOT NULL,
   `nev` varchar(30) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `latogatoi_csoport`
+-- Dumping data for table `latogatoi_csoportok`
 --
 
-INSERT INTO `latogatoi_csoport` (`id`, `nev`) VALUES
-(1, 'Tanuló'),
-(2, 'Tanár'),
-(3, 'Vendeg');
+INSERT INTO `latogatoi_csoportok` (`id`, `nev`) VALUES
+(4, 'Tanulók'),
+(5, 'Tanárok');
 
 --
--- Indexek a kiírt táblákhoz
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `Card`
---
-ALTER TABLE `Card`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `latogatoi_csoport` (`latogatoi_csoport`);
-
---
--- A tábla indexei `felhasznalok`
+-- Indexes for table `felhasznalok`
 --
 ALTER TABLE `felhasznalok`
   ADD PRIMARY KEY (`nev`);
 
 --
--- A tábla indexei `latogatoi_csoport`
+-- Indexes for table `kartyak`
 --
-ALTER TABLE `latogatoi_csoport`
+ALTER TABLE `kartyak`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `latogatoi_csoport` (`latogatoi_csoport`);
+
+--
+-- Indexes for table `latogatoi_csoportok`
+--
+ALTER TABLE `latogatoi_csoportok`
   ADD PRIMARY KEY (`id`);
 
 --
--- A kiírt táblák AUTO_INCREMENT értéke
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT a táblához `Card`
+-- AUTO_INCREMENT for table `kartyak`
 --
-ALTER TABLE `Card`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `kartyak`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT a táblához `latogatoi_csoport`
+-- AUTO_INCREMENT for table `latogatoi_csoportok`
 --
-ALTER TABLE `latogatoi_csoport`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `latogatoi_csoportok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- Megkötések a kiírt táblákhoz
+-- Constraints for dumped tables
 --
 
 --
--- Megkötések a táblához `Card`
+-- Constraints for table `kartyak`
 --
-ALTER TABLE `Card`
-  ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`latogatoi_csoport`) REFERENCES `latogatoi_csoport` (`id`);
+ALTER TABLE `kartyak`
+  ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`latogatoi_csoport`) REFERENCES `latogatoi_csoportok` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
